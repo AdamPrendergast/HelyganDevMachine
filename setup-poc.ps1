2f72d7f35c9bf1757f56500689ae9f0de1d36aa3
Adam Prendergast <adam.prendergast@thisisabsurd.com>
2022-08-12 12:21:28 +0100

    Updated poc setup

--- setup-poc.ps1
+++ setup-poc.ps1
@@ -7,3 +7,41 @@ Set-ExecutionPolicy Unrestricted
 iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 
 Get-PackageProvider -Name chocolatey -Force
+Get-PackageProvider -Name nuget -Force
+
+$env:ChocolateyInstall = Convert-Path "$((Get-Command choco).Path)\..\.."   
+Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+
+# Install Box Starter
+choco install -y boxstarter
+refreshenv
+
+#--- Enable developer mode on the system ---
+Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\AppModelUnlock -Name AllowDevelopmentWithoutDevLicense -Value 1
+
+#--- Configuring Windows properties ---
+#--- Windows Features ---
+# Show hidden files, Show protected OS files, Show file extensions
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
+
+#--- File Explorer Settings ---
+# will expand explorer to the actual folder you're in
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneExpandToCurrentFolder -Value 1
+#adds things back in your left pane like recycle bin
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -Value 1
+#opens PC to This PC, not quick access
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Value 1
+#taskbar where window is open for multi-monitor
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name MMTaskbarMode -Value 2
+
+# Install Software
+choco install -y vscode
+choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
+choco install -y 7zip.install
+
+refreshenv
+
+cd C:/
+mkdir projects
+cd projects
+git clone https://github.com/AdamPrendergast/HelyganDevMachine.git
